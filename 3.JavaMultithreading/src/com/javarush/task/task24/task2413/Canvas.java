8053b675bba1523653ef3fed7430c4a96f97d209
Igor <igor_pol04@mail.ru>
2018-11-23 21:38:48 +0500

    Арканоид(10) Что мы будем делать с Canvas? Мы будем рисовать на нем (в его матрице). Поэтому нам понадобятся два метода: void setPoint(double x, double y, char c) void drawMatrix(double x, double y, int[][] matrix, char c)
    Первый метод - setPoint будет "ставить точку в координатах x,y цветом c".
    
    В методе надо:
    а) округлить x и y до целых чисел
    б) занести в matrix[y][x] значение с
    в) ничего не делать, если x < 0 или y < 0 или y >= matrix.length или x >= matrix[0].length
    
    Второй метод - drawMatrix копирует переданную ему картинку (матрицу) в матрицу Canvas.
    И не просто копирует, а начиная с координат x, y
    
    В методе надо:
    а) с помощью двух вложенных циклов пройтись по всем ячейкам переданной картинки
    б) если значение ячейки элемента [i][j] полученной матрицы не равно 0, то покрасить в матрице объекта Canvas точку (x+j, y+i) в цвет c:
    setPoint(x+j, y+i, c)
    
    Требования:
    1. В классе Canvas должен быть создан метод void setPoint(double x, double y, char c).
    2. В классе Canvas должен быть создан метод void drawMatrix(double x, double y, int[][] matrix, char c).
    3. Поведение метода setPoint должно соответствовать условию задачи.
    4. Поведение метода drawMatrix должно соответствовать условию задачи.

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -34,4 +34,20 @@ public class Canvas {
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
+
+    void setPoint(double x, double y, char c){
+        if (x >= 0 && y >= 0 && x < matrix[0].length && y < matrix.length){
+            matrix[(int)Math.round(y)][(int)Math.round(x)] = c;
+        }
+    }
+
+    void drawMatrix(double x, double y, int[][] matrix, char c){
+        for (int i = 0, n = matrix.length; i < n; i++) {
+            for (int j = 0, m = matrix[i].length; j < m; j++) {
+                if (matrix[i][j] != 0){
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
 }
