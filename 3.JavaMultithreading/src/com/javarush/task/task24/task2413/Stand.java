f22b18df1128567cc686d8bcc60c0793e91e864f
Igor <igor_pol04@mail.ru>
2018-11-24 16:24:22 +0500

    Арканоид(17) Еще подставке нужны методы: а) move - см. move в BaseObject Движение доски осуществляется горизонтально, поэтому мы меняем только координату х. Подумай, как координата х зависит от направления (direction) и скорости (speed). Реализуй зависимость. б) draw - см. draw в BaseObject Его кодом я займусь сам. в) moveLeft() - задает постоянное движение "подставки" влево Просто присвой правильное значение переменной direction. г) moveRight() - задает постоянное движение "подставки" вправо Просто присвой правильное значение переменной direction.
    Требования:
    1. В классе Stand должен быть реализован метод move увеличивающий координату x на произведение значений полей speed и direction.
    2. В классе Stand должен быть реализован метод moveLeft, устанавливающий значение поля direction равным -1.
    3. В классе Stand должен быть реализован метод moveRight, устанавливающий значение поля direction равным 1.

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -31,6 +31,14 @@ public class Stand extends BaseObject{
 
     @Override
     public void move() {
+        x += speed * direction;
+    }
 
+    public void moveLeft(){
+        direction = -1;
+    }
+
+    public void moveRight(){
+        direction = 1;
     }
 }
