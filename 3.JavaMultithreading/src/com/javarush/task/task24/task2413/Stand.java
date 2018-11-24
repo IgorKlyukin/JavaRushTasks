a3bfd477276dee7fdbd712872fcf27689e55343a
Igor <igor_pol04@mail.ru>
2018-11-24 10:49:50 +0500

    Арканоид(16) И наконец "подставка"! Ей понадобятся такие приватные поля: а) speed (скорость шарика) типа double; б) direction (направление движения по оси x: 1 - вправо, -1 - влево, 0 - начальное значение, стоим на месте) типа double.
    Также создай для них геттеры.
    
    А еще с тебя конструктор, примерно вот такой:
    public Stand(double x, double y) {
    super(x, y, 3);
    speed = 1;
    direction = 0;
    }
    
    Требования:
    1. В классе Stand должно быть создано приватное поле speed типа double.
    2. В классе Stand должно быть создано приватное поле direction типа double.
    3. В классе Stand должен быть реализован публичный геттер для поля speed.
    4. В классе Stand должен быть реализован публичный геттер для поля direction.
    5. В классе Stand должен быть реализован конструктор с двумя параметрами типа double в соответствии с условием задачи.

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -1,8 +1,27 @@
 package com.javarush.task.task24.task2413;
 
 public class Stand extends BaseObject{
+
+    private double speed;
+    private double direction;//направление движения по оси x: 1 - вправо, -1 - влево,
+                            // 0 - начальное значение, стоим на месте
+
     public Stand(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 
     @Override
