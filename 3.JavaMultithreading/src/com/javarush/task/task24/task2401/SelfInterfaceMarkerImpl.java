eb07e86a32bc6c470420b71a6de0a1153588a34e
Igor <igor_pol04@mail.ru>
2018-12-05 21:59:33 +0500

    Создание своего интерфейса-маркера 1. Создай интерфейс-маркер SelfInterfaceMarker. 2. Создай класс SelfInterfaceMarkerImpl, который реализует SelfInterfaceMarker. 3. Добавь в SelfInterfaceMarkerImpl минимум 2 любых public метода. 4. Создай исключение UnsupportedInterfaceMarkerException, унаследуй его от класса Exception. 5. В методе testClass класса Util: если параметр == null, то выбросьте UnsupportedInterfaceMarkerException.
    Требования:
    1. Интерфейс-маркер SelfInterfaceMarker должен быть создан в отдельном файле.
    2. Класс SelfInterfaceMarkerImpl должен быть создан в отдельном файле и реализовывать интерфейс SelfInterfaceMarker.
    3. В классе SelfInterfaceMarkerImpl должны существовать минимум два public метода.
    4. Исключение UnsupportedInterfaceMarkerException должно быть создано в отдельном файле.
    5. Метод testClass класса Util должен бросать исключение UnsupportedInterfaceMarkerException в случае, если полученный параметр равен null.
    6. В интерфейсе SelfInterfaceMarker не должны быть объявлены методы или декларированы константы.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
