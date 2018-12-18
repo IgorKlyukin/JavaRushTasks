44cd636770c1f9ba41368d2ba337d2646630e0cc
Igor <igor_pol04@mail.ru>
2018-12-18 21:39:20 +0500

    Не валять дурака Восстанови логику класса TaskManipulator.
    Требования:
    1. Класс TaskManipulator должен реализовывать интерфейсы Runnable и CustomThreadManipulator.
    2. Метод run должен каждые 100 миллисекунд выводить имя исполняемой нити в консоль.
    3. Класс TaskManipulator должен иметь внутреннее поле типа Thread.
    4. Метод start должен создавать, сохранять во внутреннее поле и запускать нить с именем, которое передано через аргумент метода.
    5. Метод stop должен прерывать последнюю созданную классом TaskManipulator нить.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task25/task2508/TaskManipulator.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task25.task2508;
+
+
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    Thread thread;
+    @Override
+    public void run() {
+        while (!thread.isInterrupted()){
+            try{
+                System.out.println(thread.getName());
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e){
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        thread = new Thread(this);
+        thread.setName(threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
+    }
+}
