b2111d68ec4e5b3b913a3d88e6a52027358e76ea
Igor <igor_pol04@mail.ru>
2018-12-23 21:48:28 +0500

    Мир не меняется, меняемся мы Разберись с ConcurrentHashMap. В отдельном файле создайте класс Producer, который будет: 1. каждые полсекунды добавлять в ConcurrentHashMap ключ и значение, где ключ - счетчик начиная с 1, значение - фраза: "Some text for i" , пример "Some text for 1". 2. при возникновении исключения выводить в консоль: "[THREAD_NAME] thread was terminated", пример "[thread-1] thread was terminated".
    Требования:
    1. Класс Producer должен быть создан в отдельном файле.
    2. Класс Producer должен реализовывать интерфейс Runnable.
    3. Класс Producer должен содержать приватное поле ConcurrentHashMap<String, String> map.
    4. Класс Producer должен содержать конструктор с одним параметром, инициализирующий поле map.
    5. Метод run() класса Producer должен каждые полсекунды добавлять в ConcurrentHashMap ключ и значение согласно заданию.
    6. Метод run() класса Producer должен при возникновении исключения выводить в консоль "[TREAD_NAME] thread was terminated".

---
+++ 3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Producer implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap<String, String> map) {
+        this.map = map;
+    }
+
+    public void run() {
+        try {
+            Thread currentThread = Thread.currentThread();
+            int i = 1;
+            while (!currentThread.isInterrupted()) {
+                map.put("" + i, "Some text for " + i++);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
