7f4a59af57913d338fa385510484953dff89e550
Igor <igor_pol04@mail.ru>
2018-12-24 19:18:48 +0500

    Чат (3) Прежде, чем двигаться дальше, нужно разработать протокол общения клиента и сервера.
    Сформулируем основные моменты протокола:
    - Когда новый клиент хочет подсоединиться к серверу, сервер должен запросить имя
    клиента.
    - Когда клиент получает запрос имени от сервера он должен отправить свое имя серверу.
    - Когда сервер получает имя клиента он должен принять это имя или запросить новое.
    - Когда новый клиент добавился к чату, сервер должен сообщить остальным участникам о
    новом клиенте.
    - Когда клиент покидает чат, сервер должен сообщить остальным участникам об этом.
    - Когда сервер получает текстовое сообщение от клиента, он должен переслать его всем
    остальным участникам чата.
    
    Добавь для каждого пункта вышеописанного протокола соответствующее значение в enum
    MessageType:
    1) NAME_REQUEST - запрос имени.
    2) USER_NAME - имя пользователя.
    3) NAME_ACCEPTED - имя принято.
    4) TEXT - текстовое сообщение.
    5) USER_ADDED - пользователь добавлен.
    6) USER_REMOVED - пользователь удален.
    
    Требования:
    1. В перечислении MessageType должен присутствовать элемент NAME_REQUEST.
    2. В перечислении MessageType должен присутствовать элемент USER_NAME.
    3. В перечислении MessageType должен присутствовать элемент NAME_ACCEPTED.
    4. В перечислении MessageType должен присутствовать элемент TEXT.
    5. В перечислении MessageType должен присутствовать элемент USER_ADDED.
    6. В перечислении MessageType должен присутствовать элемент USER_REMOVED.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -1,4 +1,24 @@
 package com.javarush.task.task30.task3008;
 
 public enum MessageType {
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED;
+//    NAME_REQUEST ("запрос имени"),
+//    USER_NAME ("имя пользователя"),
+//    NAME_ACCEPTED ("имя принято"),
+//    TEXT ("текстовое сообщение"),
+//    USER_ADDED ("пользователь добавлен"),
+//    USER_REMOVED ("пользователь удален");
+//
+//    private String description;
+//
+//    private MessageType(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getDescription() {return description;}
 }
