b45b941094c757a9880a550a8d9888e84700a53f
Igor <igor_pol04@mail.ru>
2018-12-24 21:36:12 +0500

    Чат (6) Приступим к самому важному - написанию класса Server. Сервер должен поддерживать
    множество соединений с разными клиентами одновременно. Это можно реализовать с
    
    помощью следующего алгоритма:
    
    - Сервер создает серверное сокетное соединение.
    
    - В цикле ожидает, когда какой-то клиент подключится к сокету.
    
    - Создает новый поток обработчик Handler, в котором будет происходить обмен
    
    сообщениями с клиентом.
    
    - Ожидает следующее соединение.
    
    Добавь:
    
    1)	В класс Server приватный статический вложенный класс Handler, унаследованный от
    
    Thread.
    
    2)	В класс Handler поле socket типа Socket.
    
    3)	В класс Handler конструктор, принимающий в качестве параметра Socket и
    
    инициализирующий им соответствующее поле класса.
    
    4)	Метод main класса Server, должен:
    
    а) Запрашивать порт сервера, используя ConsoleHelper.
    
    б) Создавать серверный сокет java.net.ServerSocket, используя порт из предыдущего пункта.
    
    в) Выводить сообщение, что сервер запущен.
    
    г) В бесконечном цикле слушать и принимать входящие сокетные соединения только что созданного
    
    серверного сокета.
    
    д) Создавать и запускать новый поток Handler, передавая в конструктор сокет из предыдущего пункта.
    
    е) После создания потока обработчика Handler переходить на новый шаг цикла.
    
    ж) Предусмотреть закрытие серверного сокета в случае возникновения исключения.
    
    з) Если исключение Exception все же произошло, поймать его и вывести сообщение
    
    об ошибке.
    
    Требования:
    1. В классе Server должен быть создан приватный статический класс Handler, унаследованный от класса Thread.
    2. В классе Handler должно быть создано поле socket типа Socket.
    3. Конструктор класса Handler должен принимать один параметр типа Socket и инициализировать поле socket.
    4. Метод main должен считывать с клавиатуры порт сервера используя метод readInt класса ConsoleHelper.
    5. Метод main должен корректно реализовывать бесконечный цикл описанный в условии задачи.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,29 @@
 package com.javarush.task.task30.task3008;
 
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-        
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер запущен!");
+            while (true){
+                new Handler(serverSocket.accept()).start();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static class Handler extends Thread {
+        Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
