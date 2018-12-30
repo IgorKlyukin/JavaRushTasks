e1fd9a6608f9eb49f41251b9c67e4b62c964574c
Igor <igor_pol04@mail.ru>
2018-12-30 09:44:11 +0500

    Чат (19) Сейчас будем реализовывать класс BotSocketThread, вернее переопределять некоторые его методы, весь основной функционал он уже унаследовал от SocketThread.
    1. Переопредели метод clientMainLoop():
    а) С помощью метода sendTextMessage() отправь сообщение с текстом "Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды."
    б) Вызови реализацию clientMainLoop() родительского класса.
    2. Переопредели метод processIncomingMessage(String message). Он должен следующим образом обрабатывать входящие сообщения:
    а) Вывести в консоль текст полученного сообщения message.
    б) Получить из message имя отправителя и текст сообщения. Они разделены ": ".
    в) Отправить ответ в зависимости от текста принятого сообщения.
    Если текст сообщения:
    "дата" - отправить сообщение содержащее текущую дату в формате "d.MM.YYYY";
    "день" - в формате "d";
    "месяц" - "MMMM";
    "год" - "YYYY";
    "время" - "H:mm:ss";
    "час" - "H";
    "минуты" - "m";
    "секунды" - "s".
    Указанный выше формат используй для создания объекта SimpleDateFormat. Для получения текущей даты необходимо использовать класс Calendar и метод getTime().
    Ответ должен содержать имя клиента, который прислал запрос и ожидает ответ, например, если Боб отправил запрос "время", мы должны отправить ответ "Информация для Боб: 12:30:47".
    Наш бот готов. Запусти сервер, запусти бота, обычного клиента и убедись, что все работает правильно.
    Помни, что message бывают разных типов и не всегда содержат ":"
    
    Требования:
    1. Метод clientMainLoop класса BotSocketThread должен вызывать метод sendTextMessage у внешнего объекта BotClient c приветственным сообщением указанном в условии задачи.
    2. Метод clientMainLoop класса BotSocketThread должен вызывать clientMainLoop у объекта родительского класса (super).
    3. Метод processIncomingMessage должен выводить на экран полученное сообщение message.
    4. Метод processIncomingMessage должен отправлять ответ с помощью метода sendTextMessage (форматирование согласно условию задачи).
    5. Метод processIncomingMessage не должен вызывать метод sendTextMessage для некорректных запросов.

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task30.task3008.client;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class BotClient extends Client {
 
     public static void main(String[] args) {
@@ -7,6 +11,64 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends SocketThread {
+
+        public BotSocketThread() {
+        }
+
+        @Override
+        protected void processIncomingMessage(String message) {
+            if (message.contains(": ")) {
+                String[] strings = message.split(": ", 2);
+                SimpleDateFormat simpleDateFormat = null;
+                switch (strings[1]){
+                    case "дата": {
+                        simpleDateFormat = new SimpleDateFormat("d.MM.YYYY");
+                        break;
+                    }
+                    case "день": {
+                        simpleDateFormat = new SimpleDateFormat("d");
+                        break;
+                    }
+                    case "месяц": {
+                        simpleDateFormat = new SimpleDateFormat("MMMM");
+                        break;
+                    }
+                    case "год": {
+                        simpleDateFormat = new SimpleDateFormat("YYYY");
+                        break;
+                    }
+                    case "время": {
+                        simpleDateFormat = new SimpleDateFormat("H:mm:ss");
+                        break;
+                    }
+                    case "час": {
+                        simpleDateFormat = new SimpleDateFormat("H");
+                        break;
+                    }
+                    case "минуты": {
+                        simpleDateFormat = new SimpleDateFormat("m");
+                        break;
+                    }
+                    case "секунды": {
+                        simpleDateFormat = new SimpleDateFormat("s");
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+                if (simpleDateFormat != null) {
+                    sendTextMessage("Информация для " + strings[0] + ": " + simpleDateFormat.format(Calendar.getInstance().getTime()));
+                }
+            }
+            super.processIncomingMessage(message);
+        }
+
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
     }
 
     @Override
