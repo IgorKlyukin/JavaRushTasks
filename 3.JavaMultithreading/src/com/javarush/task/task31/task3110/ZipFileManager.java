8af256958cd65d19f02522910188d2d4790241ca
Igor <igor_pol04@mail.ru>
2019-01-01 23:38:48 +0500

    Archiver (11) Давай наконец попробуем нормально реализовать метод execute() класса ZipCreateCommand, который мы добавляли раньше.
    Для этого нужно:
    1. В начале метода добавить вывод сообщения "Создание архива." Не забудь, что мы работаем с консолью через методы класса ConsoleHelper.
    2. Создай новый объект класса ZipFileManager. Т.к. создание этого объекта будет необходимо и другим командам, вынеси создание в отдельный метод ZipFileManager getZipFileManager() throws Exception в класс ZipCommand.
    Этот метод должен:
    2.1. Просить пользователя ввести полный путь файла архива
    2.2. Считывать введенный путь в переменную типа String
    2.3. Используя введенный String формировать путь Path
    2.4. Создать объект ZipFileManager, передав в конструктор полученный путь
    2.5. Вернуть созданный объект
    3. Попроси пользователя ввести полное имя файла или директории для архивации
    4. Создай путь Path, используя введенную строку
    5. У объекта класса ZipFileManager вызови метод createZip(), передав в него путь из п.4
    6. Выведи в консоль сообщение "Архив создан."
    7. Оберни содержимое метода execute() класса ZipCreateCommand в блок try-catch и отлавливай
    исключение типа PathIsNotFoundException. Если оно произошло, выведи сообщение "Вы неверно указали имя файла или директории."
    8. Запусти программу и проверь, что команда "упаковать файлы в архив" работает
    
    Требования:
    1. В классе ZipCommand нужно добавить public метод ZipFileManager getZipFileManager() throws Exception.
    2. Метод getZipFileManager должен считывать с консоли путь к архиву и возвращать для него объект ZipFileManager.
    3. Метод execute в классе ZipCreateCommand должен быть реализован следуя описанию в задании, используя метод getZipFileManager.
    4. В случае возникновения исключения PathIsNotFoundException, нужно вывести в консоль сообщение "Вы неверно указали имя файла или директории.".

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -2,57 +2,71 @@ package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 
-import java.io.File;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
-    private Path zipFile;
+    // Полный путь zip файла
+    private final Path zipFile;
 
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
     }
 
     public void createZip(Path source) throws Exception {
-        if (Files.notExists(zipFile.getParent())) {
-            Files.createDirectory(zipFile.getParent());
-        }
+        // Проверяем, существует ли директория, где будет создаваться архив
+        // При необходимости создаем ее
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory))
+            Files.createDirectories(zipDirectory);
+
+        // Создаем zip поток
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-            if (Files.isRegularFile(source)) {
-                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
-            }
-            else if (Files.isDirectory(source)) {
-                FileManager fileManager  = new FileManager(source);
+
+            if (Files.isDirectory(source)) {
+                // Если архивируем директорию, то нужно получить список файлов в ней
+                FileManager fileManager = new FileManager(source);
                 List<Path> fileNames = fileManager.getFileList();
-                for (Path fileName:
-                     fileNames) {
+
+                // Добавляем каждый файл в архив
+                for (Path fileName : fileNames)
                     addNewZipEntry(zipOutputStream, source, fileName);
-                }
-            }
-            else {
+
+            } else if (Files.isRegularFile(source)) {
+
+                // Если архивируем отдельный файл, то нужно получить его директорию и имя
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else {
+
+                // Если переданный source не директория и не файл, бросаем исключение
                 throw new PathIsNotFoundException();
             }
         }
     }
 
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
-        try (InputStream inputStream = Files.newInputStream(Paths.get(filePath.toString(), fileName.toString()))) {
-            ZipEntry zipEntry = new ZipEntry(fileName.toString());
-            zipOutputStream.putNextEntry(zipEntry);
+        Path fullPath = filePath.resolve(fileName);
+        try (InputStream inputStream = Files.newInputStream(fullPath)) {
+            ZipEntry entry = new ZipEntry(fileName.toString());
+
+            zipOutputStream.putNextEntry(entry);
+
             copyData(inputStream, zipOutputStream);
+
             zipOutputStream.closeEntry();
         }
     }
 
     private void copyData(InputStream in, OutputStream out) throws Exception {
-        while (in.available() > 0) {
-            out.write(in.read());
+        byte[] buffer = new byte[8 * 1024];
+        int len;
+        while ((len = in.read(buffer)) > 0) {
+            out.write(buffer, 0, len);
         }
     }
 }
