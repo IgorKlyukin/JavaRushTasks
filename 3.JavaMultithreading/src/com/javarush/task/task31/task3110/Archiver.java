729405e708c386ed04e9e322dba89e95a42b77d1
Igor <igor_pol04@mail.ru>
2019-01-01 17:13:30 +0500

    Archiver (8) Чтобы узнать какую команду сейчас хочет выполнить пользователь, добавим метод Operation askOperation() в класс Archiver. Этот метод должен вывести в консоль список доступных команд и попросить выбрать одну из них. Для удобства будем просить ввести номер команды, где номер - это порядковый номер команды в enum Operation. Получить порядковый номер значения в enum'е можно с помощью метода ordinal(). Теперь все готово чтобы переписать main, используя последние достижения науки и техники, а именно класс CommandExecutor и метод askOperation(). 1. Добавь публичный статический метод Operation askOperation() throws IOException в класс Archiver. Он должен: 1.1. Использовать методы класса ConsoleHelper 1.2. Запрашивать у пользователя номер операции, которую он хочет совершить.
    Подсказка:
    чтобы вывести номер операции "Создать архив", используй: Operation.CREATE.ordinal()
    
    1.3. Возвращать выбранную операцию.
    
    Пример вывода метода askOperation():
    Выберите операцию:
    0 - упаковать файлы в архив
    1 - добавить файл в архив
    2 - удалить файл из архива
    3 - распаковать архив
    4 - просмотреть содержимое архива
    5 - выход
    2. Перепиши метод main():
    2.1. Объяви локальную переменную типа Operation
    2.2. В цикле запрашивай новое значение для переменной п.2.1. с помощью метода askOperation() и вызывай выполнение операции с помощью CommandExecutor.execute()
    2.3. Обеспечь выход из цикла, если пользователь выбрал операцию Operation.EXIT
    2.4. Оберни вызов askOperation() и execute(operation) в блок try-catch. Если произойдет исключение WrongZipFileException выведи сообщение "Вы не выбрали файл архива или выбрали неверный файл." с помощью ConsoleHelper, при любых других исключениях выводи "Произошла ошибка. Проверьте введенные данные.".
    2.5. Проследи, чтобы программа продолжила свою работу (перешла на новый шаг цикла), после обработки исключений.
    3. Запусти программу и проверь, что команда "выход" работает.
    
    Требования:
    1. В классе Archiver нужно создать публичный статический метод Operation askOperation().
    2. Метод askOperation должен вывести в консоль все возможные операции и их номер.
    3. Метод askOperation должен считать с клавиатуры номер выбранной операции и вернуть ее.
    4. Перепиши метод main согласно заданию.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,26 +10,52 @@ import java.nio.file.Paths;
 
 public class Archiver {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String pathZip, pathFile;
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        String pathZip, pathFile;
+        Operation operation = null;
 //        do {
-            System.out.print("Full pathZip: ");
-            pathZip = reader.readLine();
+//            System.out.print("Full pathZip: ");
+//            pathZip = reader.readLine();
 //        }while (!pathZip.matches(".:\\\\[^\\\\]+.*(\\.zip)$"));
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathZip));
+//        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathZip));
 
 
 //        do {
-            System.out.print("Full pathFile: ");
-            pathFile = reader.readLine();
+//            System.out.print("Full pathFile: ");
+//            pathFile = reader.readLine();
 //        }while (!pathFile.matches(".:\\\\[^\\\\]+.*"));
-        try {
-            zipFileManager.createZip(Paths.get(pathFile));
-            new ExitCommand().execute();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            zipFileManager.createZip(Paths.get(pathFile));
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            }
+            catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            }
+            catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        }while (!Operation.EXIT.equals(operation));
+
+
+    }
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 
 }
