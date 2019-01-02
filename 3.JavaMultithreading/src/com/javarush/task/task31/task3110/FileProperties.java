6b81cd621d59d0f6843f97c16505b83f45dd07a9
Igor <igor_pol04@mail.ru>
2019-01-02 13:50:14 +0500

    Archiver (13) Продолжим наш путь к получению содержимого файла архива. Напишем метод getFilesList() в классе ZipFileManager. Он будет возвращать список файлов в архиве, вернее список свойств этих файлов (класс свойств FileProperties мы уже реализовали). Итак: 1. Добавь метод List<FileProperties> getFilesList() throws Exception в класс ZipFileManager 2. Внутри метода проверь является ли содержимое zipFile обычным файлом с помощью подходящего метода класса Files. Если это не файл, брось исключение WrongZipFileException(). 3. Создай список с элементами типа FileProperties, в него мы будем складывать свойства файлов 4. Создай входящий поток ZipInputStream, для файла из переменной zipFile. Как и в прошлые разы, оберни его создание в try-with-resources 5. Пройдись по всем элементам ZipEntry потока ZipInputStream 6. Для каждого элемента ZipEntry вычитай его содержимое, иначе у нас не будет информации о его размере. Нельзя узнать размер файла в архиве, не вычитав его. Это очень легко сделать с помощью функции copyData, используя временный буфер типа ByteArrayOutputStream. 7. Получи имя, размер, сжатый размер и метод сжатия элемента архива. Посмотри, что еще можно узнать о нем. 8. Создай объект класса FileProperties, используя полученные данные о файле. 9. Добавь созданный объект из п.8 в список из п.3 10. После выхода из цикла верни собранную информацию вызвавшему методу.
    Требования:
    1. В классе ZipFileManager нужно создать публичный метод List<FileProperties> getFilesList() throws Exception.
    2. Если Path zipFile не является файлом, метод getFilesList должен кинуть исключение WrongZipFileException.
    3. Метод getFilesList должен пройтись по всем файлам архива и вернуть данные о них. Реализация должна быть выполнена согласно описанию в задании.
    4. Поток для чтения из архива должен быть закрыт.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileProperties.java
@@ -13,6 +13,7 @@ public class FileProperties {
         this.compressionMethod = compressionMethod;
     }
 
+
     public String getName() {
         return name;
     }
@@ -30,16 +31,25 @@ public class FileProperties {
     }
 
     public long getCompressionRatio() {
+        // Вычисляем степень сжатия
         return 100 - ((compressedSize * 100) / size);
     }
 
     @Override
     public String toString() {
+        // Строим красивую строку из свойств
+        StringBuilder builder = new StringBuilder();
+        builder.append(name);
         if (size > 0) {
-            return String.format("%s %d Kb (%d Kb) сжатие: %d%%", name, size / 1024, compressedSize / 1024, getCompressionRatio());
+            builder.append("\t");
+            builder.append(size / 1024);
+            builder.append(" Kb (");
+            builder.append(compressedSize / 1024);
+            builder.append(" Kb) сжатие: ");
+            builder.append(getCompressionRatio());
+            builder.append("%");
         }
-        else {
-            return name;
-        }
+
+        return builder.toString();
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,13 +1,18 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
@@ -67,6 +72,24 @@ public class ZipFileManager {
         int len;
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
+        }
+    }
+
+    public List<FileProperties> getFilesList() throws Exception {
+        if (Files.isDirectory(zipFile)) {
+            throw new WrongZipFileException();
+        }
+        else {
+            List<FileProperties> fileProperties = new ArrayList<>();
+            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+                ZipEntry zipEntry;
+                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                    copyData(zipInputStream, byteArrayOutputStream);
+                    fileProperties.add(new FileProperties(zipEntry.getName(), byteArrayOutputStream.size(), zipEntry.getCompressedSize(), zipEntry.getMethod()));
+                }
+            }
+            return fileProperties;
         }
     }
 }
