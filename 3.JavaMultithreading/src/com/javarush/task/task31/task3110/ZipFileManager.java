2c34215f2929e979c56168cf9581fccc7c3dc826
Igor <igor_pol04@mail.ru>
2019-01-02 14:15:23 +0500

    Archiver (14) Все готово, чтобы реализовать метод execute() класса ZipContentCommand: 1. Выведи сообщение "Просмотр содержимого архива." 2. Создай объект класса ZipFileManager с помощью метода getZipFileManager() 3. Выведи сообщение "Содержимое архива:" 4. Получи список файлов архива с помощью метода getFilesList() 5. Выведи свойства каждого файла в консоль. Тут нам и пригодится ранее реализованный метод toString() класса FileProperties 6. Выведи сообщение "Содержимое архива прочитано." 7. Запусти программу и проверь, что команда "просмотреть содержимое архива" работает
    Требования:
    1. В методе execute класса ZipContentCommand нужно получить объект ZipFileManager.
    2. В методе execute класса ZipContentCommand у объекта ZipFileManager нужно получить список файлов в архиве.
    3. В методе execute класса ZipContentCommand нужно вывести свойства каждого файла в архиве на консоль.

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -11,7 +11,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
@@ -54,6 +53,32 @@ public class ZipFileManager {
         }
     }
 
+    public List<FileProperties> getFilesList() throws Exception {
+        // Проверяем существует ли zip файл
+        if (!Files.isRegularFile(zipFile)) {
+            throw new WrongZipFileException();
+        }
+
+        List<FileProperties> files = new ArrayList<>();
+
+        try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
+            ZipEntry zipEntry = zipInputStream.getNextEntry();
+
+            while (zipEntry != null) {
+                // Поля "размер" и "сжатый размер" не известны, пока элемент не будет прочитан
+                // Давайте вычитаем его в какой-то выходной поток
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                copyData(zipInputStream, baos);
+
+                FileProperties file = new FileProperties(zipEntry.getName(), zipEntry.getSize(), zipEntry.getCompressedSize(), zipEntry.getMethod());
+                files.add(file);
+                zipEntry = zipInputStream.getNextEntry();
+            }
+        }
+
+        return files;
+    }
+
     private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
         Path fullPath = filePath.resolve(fileName);
         try (InputStream inputStream = Files.newInputStream(fullPath)) {
@@ -72,24 +97,6 @@ public class ZipFileManager {
         int len;
         while ((len = in.read(buffer)) > 0) {
             out.write(buffer, 0, len);
-        }
-    }
-
-    public List<FileProperties> getFilesList() throws Exception {
-        if (Files.isDirectory(zipFile)) {
-            throw new WrongZipFileException();
-        }
-        else {
-            List<FileProperties> fileProperties = new ArrayList<>();
-            try (ZipInputStream zipInputStream = new ZipInputStream(Files.newInputStream(zipFile))) {
-                ZipEntry zipEntry;
-                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                    ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                    copyData(zipInputStream, byteArrayOutputStream);
-                    fileProperties.add(new FileProperties(zipEntry.getName(), byteArrayOutputStream.size(), zipEntry.getCompressedSize(), zipEntry.getMethod()));
-                }
-            }
-            return fileProperties;
         }
     }
 }
