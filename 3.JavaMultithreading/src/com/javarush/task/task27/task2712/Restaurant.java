173d8f89fd03cf7ac4f8130f9ab3c93e54dfccda
Igor <igor_pol04@mail.ru>
2019-01-06 17:50:08 +0500

    Ресторан(3) Сейчас мы можем создавать заказы. Давай сделаем так, чтобы они автоматически поступали к повару. Есть много различных способов реализации данной функциональности. Почитай про паттерн Observer - http://en.wikipedia.org/wiki/Observer_pattern Он уже реализован в java, и мы его будем использовать.
    1. Создадим класс Cook(Повар) в пакете kitchen, он будет готовить. Пусть в конструкторе приходит его имя, которое выводится методом toString.
    
    2. Tablet создает заказы, а Cook их обрабатывает. Расставь правильно Observer и Observable между Tablet и Cook.
    
    3. Метод void update(Observable observable, Object arg), который необходимо реализовать, принимает два параметра.
    - observable - объект, который отправил нам значение
    - arg - само значение, в нашем случае - это объект Order
    На данном этапе мы будем лишь имитировать обработку и выведем в консоль "Start cooking - " + order
    
    3. Пишем main.
    Для объекта Observable добавляем свой объект Observer. См. п.2 и описание паттерна в wikipedia
    Называем повара, имя не влияет на тесты. В моем варианте - это Amigo : )
    
    Сверим выводы в консоль. Пример моего вывода:
    Your order: [Soup] of Tablet{number=5}
    Start cooking - Your order: [Soup] of Tablet{number=5}
    
    4. Не забудь сразу после создания заказа и вывода информации о нем в консоль (найдите это место в коде) сделать следующее:
    4.1. Установить флаг setChanged()
    4.2. Отправить обсерверу заказ - notifyObservers(order);
    
    5. Также внесем небольшое изменение. Сделай так чтобы метод createOrder возвращал текущий заказ или null, если заказ создать не удалось.
    
    Требования:
    1. Конструктор класса Cook должен принимать в качестве параметра строку и инициализировать имя повара (поле name).
    2. Метод toString класса Cook должен возвращать значение поля name.
    3. Класс Cook должен реализовывать интерфейс Observer.
    4. Класс Tablet должен быть потомком класса Observable.
    5. В методе createOrder класса Tablet должен быть вызван метод setChanged.
    6. В методе createOrder класса Tablet должен быть вызван метод notifyObservers с текущим заказом в качестве параметра.
    7. Метод update в классе Cook должен выводить на экран строку форматированную согласно заданию.
    8. В методе main класса Restaurant должен быть создан новый повар и добавлен планшету в качестве наблюдателя с помощью метода addObserver.

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Restaurant.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
+
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(999);
+        Cook cook = new Cook("Amigo");
+        tablet.addObserver(cook);
         for (int i = 0; i < 4; i++) {
             tablet.createOrder();
         }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,24 +3,33 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable{
     private final int number;
+    private Order order;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    public void createOrder() {
+    public Order createOrder() {
         try {
-            new Order(this);
+            order = new Order(this);
+            ConsoleHelper.writeMessage(order.toString());
+            setChanged();
+            notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "Console is unavailable.");
+            order = null;
         }
+        return order;
     }
+
+
 
     @Override
     public String toString() {
