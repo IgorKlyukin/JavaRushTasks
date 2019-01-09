a15b691b57656d271292e8f6c913c41c57025d9c
Igor <igor_pol04@mail.ru>
2019-01-09 19:47:20 +0500

    Класс ConsoleReader Сделать класс ConsoleReader, у которого будут 4 статических метода: String readString() - читает с клавиатуры строку int readInt() - читает с клавиатуры число double readDouble() - читает с клавиатуры дробное число boolean readBoolean() - читает с клавиатуры строку "true" или "false" и возвращает соответствующую логическую переменную true или false
    Внимание: создавайте переменную для чтения данных с консоли (BufferedReader или Scanner) внутри каждого метода.
    
    Требования:
    1. Метод readString должен считывать и возвращать строку(тип String).
    2. Метод readInt должен считывать и возвращать число(тип int).
    3. Метод readDouble должен считывать и возвращать дробное число(тип double).
    4. Метод readBoolean должен считывать и возвращать логическую переменную(тип boolean).

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0610/ConsoleReader.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task06.task0610;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Класс ConsoleReader
+*/
+
+public class ConsoleReader {
+    public static String readString() throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return reader.readLine();
+
+    }
+
+    public static int readInt() throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(reader.readLine());
+    }
+
+    public static double readDouble() throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Double.parseDouble(reader.readLine());
+    }
+
+    public static boolean readBoolean() throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        return Boolean.parseBoolean(reader.readLine());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
