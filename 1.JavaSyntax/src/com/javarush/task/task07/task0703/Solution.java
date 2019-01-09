aaa590b9884b93c6bc1e4ce9163caf22c260f47c
Igor <igor_pol04@mail.ru>
2019-01-09 21:24:24 +0500

    Общение одиноких массивов 1. Создать массив на 10 строк. 2. Создать массив на 10 чисел. 3. Ввести с клавиатуры 10 строк, заполнить ими массив строк. 4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
    Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.
    
    Требования:
    1. Программа должна создавать массив на 10 строк.
    2. Программа должна создавать массив на 10 целых чисел.
    3. Программа должна считывать строки для массива с клавиатуры.
    4. Программа должна в массив чисел записать длины строк из массива строк, а затем их вывести на экран.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0703/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task07.task0703;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Общение одиноких массивов
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        String[] strings = new String[10];
+        int[] ints = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            strings[i] = reader.readLine();
+            ints[i] = strings[i].length();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(ints[i]);
+        }
+    }
+}
