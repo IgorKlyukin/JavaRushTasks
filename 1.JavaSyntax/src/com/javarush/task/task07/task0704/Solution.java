93f743f86122a3f497ca717c78b9eecc8ca61022
Igor <igor_pol04@mail.ru>
2019-01-09 21:30:24 +0500

    Переверни массив 1. Создать массив на 10 чисел. 2. Ввести с клавиатуры 10 чисел и записать их в массив. 3. Вывести на экран элементы массива в обратном порядке, каждое значение выводить с новой строки.
    Требования:
    1. Программа должна создавать массив на 10 целых чисел.
    2. Программа должна считывать числа для массива с клавиатуры.
    3. Программа должна выводить 10 строчек, каждую с новой строки.
    4. Массив должен быть выведен на экран в обратном порядке.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0704/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task07.task0704;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+Переверни массив
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        int[] ints = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 10; i++) {
+            ints[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 9; i >= 0; i--) {
+            System.out.println(ints[i]);
+        }
+    }
+}
+
