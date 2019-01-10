4dab8dab8d27ffc0a1e7b24ca8c23986e323605e
Igor <igor_pol04@mail.ru>
2019-01-10 19:46:13 +0500

    Вывести числа в обратном порядке Ввести с клавиатуры 10 чисел и заполнить ими список. Вывести их в обратном порядке. Каждый элемент - с новой строки. Использовать только цикл for.
    Требования:
    1. Объяви переменную типа список целых чисел и сразу проинициализируй ee.
    2. Считай 10 целых чисел с клавиатуры и добавь их в список.
    3. Выведи числа в обратном порядке.
    4. Используй цикл for.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0719/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task07.task0719;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Вывести числа в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //напишите тут ваш код
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(Integer.parseInt(reader.readLine()));
+        }
+        for (int i = arrayList.size() - 1; i >= 0 ; i--) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+}
