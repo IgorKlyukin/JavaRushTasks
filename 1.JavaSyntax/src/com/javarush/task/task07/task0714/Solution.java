ed67c04ffce6305c75d85ad1996d121f7380ff15
Igor <igor_pol04@mail.ru>
2019-01-10 08:21:56 +0500

    Слова в обратном порядке Введи с клавиатуры 5 слов в список строк. Удали 3 - ий элемент списка, и выведи оставшиеся элементы в обратном порядке.
    Требования:
    1. Объяви переменную типа ArrayList<String> (список строк) и сразу проинициализируй ee.
    2. Считай 5 строк с клавиатуры и добавь их в список.
    3. Удали третий элемент списка.
    4. Выведи элементы на экран, каждый с новой строки.
    5. Порядок вывода должен быть обратный.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0714/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task07.task0714;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/* 
+Слова в обратном порядке
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        ArrayList<String> arrayList = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            arrayList.add(reader.readLine());
+        }
+        arrayList.remove(2);
+        for (int i = arrayList.size() - 1; i >= 0 ; i--) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+}
+
+
