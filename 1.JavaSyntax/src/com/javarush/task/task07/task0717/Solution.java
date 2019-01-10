9ae94209b38a71cd12e030a0c35b39e661d50be5
Igor <igor_pol04@mail.ru>
2019-01-10 19:30:07 +0500

    Удваиваем слова 1. Введи с клавиатуры 10 слов в список строк.
    2. Метод doubleValues должен удваивать слова по принципу:
    альфа,бета,гамма -> альфа,альфа,бета,бета,гамма,гамма
    
    3. Выведи результат на экран, каждое значение с новой строки.
    
    Требования:
    1. Объяви переменную типа список строк и сразу проинициализируй ee.
    2. Считай 10 строк с клавиатуры и добавь их в список.
    3. Метод doubleValues должен удваивать элементы списка по принципу альфа,бета,гамма -> альфа,альфа,бета,бета,гамма,гамма.
    4. Выведи получившийся список на экран, каждый элемент с новой строки.

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0717/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task07.task0717;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Удваиваем слова
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        // Считать строки с консоли и объявить ArrayList list тут
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        ArrayList<String> result = doubleValues(list);
+
+        // Вывести на экран result
+        for (String s :
+                result) {
+            System.out.println(s);
+        }
+    }
+
+    public static ArrayList<String> doubleValues(ArrayList<String> list) {
+        //напишите тут ваш код
+        for (int i = 0; i < list.size(); i++)
+            list.add(i,list.get(i++));
+        return list;
+    }
+}
