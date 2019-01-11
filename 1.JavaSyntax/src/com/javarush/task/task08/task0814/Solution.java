0b3f191c3073823bbff9a4dcc1b9960e41990d9a
Igor <igor_pol04@mail.ru>
2019-01-11 20:06:28 +0500

    Больше 10? Вы нам не подходите Создать множество чисел(Set<Integer>), занести туда 20 различных чисел. Удалить из множества все числа больше 10.
    Требования:
    1. Программа не должна выводить текст на экран.
    2. Программа не должна считывать значения с клавиатуры.
    3. Класс Solution должен содержать только три метода.
    4. Метод createSet() должен создавать и возвращать множество HashSet состоящих из 20 различных чисел.
    5. Метод removeAllNumbersGreaterThan10() должен удалять из множества все числа больше 10.

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0814/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task08.task0814;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Больше 10? Вы нам не подходите
+*/
+
+public class Solution {
+    public static HashSet<Integer> createSet() {
+        // напишите тут ваш код
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 20; i++) {
+            set.add(i);
+        }
+        return (HashSet<Integer>)set;
+    }
+
+    public static HashSet<Integer> removeAllNumbersGreaterThan10(HashSet<Integer> set) {
+        // напишите тут ваш код
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()) {
+            if (iter.next() > 10) {
+                iter.remove();
+            }
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
