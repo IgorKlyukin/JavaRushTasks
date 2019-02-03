04bfcfd859e14db3d4a291862658f32c5bdc82c5
Igor <igor_pol04@mail.ru>
2019-02-03 21:46:58 +0500

    2048 (2) В игре 2048 поле состоит из 16 плиток, каждая из которых имеет определенный вес. Кроме веса у плитки еще будет собственный цвет и цвет текста которым будет отображаться вес плитки. Цвета плиток находятся в диапазоне от светло-серого до красного, а цвет текста будет зависеть от цвета плитки.
    Создадим класс Tile описывающий одну плитку.
    В нем нам понадобятся:
    1. Поле value типа int, уровень доступа по умолчанию.
    2. Конструктор с параметром, инициализирующий поле value.
    3. Конструктор без параметров (значение поля value должно быть равно нулю).
    4. Метод isEmpty, возвращающий true в случае, если значение поля value равно 0, иначе - false.
    5. Метод getFontColor, возвращающий новый цвет(объект типа Color) (0x776e65) в случае, если вес плитки меньше 16, иначе - 0xf9f6f2.
    6. Метод getTileColor, возвращающий цвет плитки в зависимости от ее веса в соответствии с нижеприведенными значениями:
    0: (0xcdc1b4);
    2: (0xeee4da);
    4: (0xede0c8);
    8: (0xf2b179);
    16: (0xf59563);
    32: (0xf67c5f);
    64: (0xf65e3b);
    128: (0xedcf72);
    256: (0xedcc61);
    512: (0xedc850);
    1024: (0xedc53f);
    2048: (0xedc22e);
    
    для любых других значений: (0xff0000).
    
    Вышеперечисленные методы не должны быть приватными.
    
    Требования:
    1. В классе Tile должно присутствовать поле value типа int с уровнем доступа по умолчанию.
    2. Конструктор класса Tile с одним параметром типа int должен инициализировать поле value.
    3. После создания объекта типа Tile с помощью конструктора без параметров, значение поля value должно быть равно нулю.
    4. Метод isEmpty должен возвращать true в случае, если значение поля value равно 0, иначе - false.
    5. Метод getFontColor должен быть реализован в соответствии с условием задачи.
    6. Метод getTileColor должен возвращать цвет плитки в зависимости от ее веса.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Tile.java
@@ -0,0 +1,43 @@
+package com.javarush.task.task35.task3513;
+
+import java.awt.*;
+
+public class Tile {
+    int value;
+
+    public Tile(int value) {
+        this.value = value;
+    }
+
+    public Tile() {
+        this.value = 0;
+    }
+
+    public boolean isEmpty() {
+        return value == 0;
+    }
+
+    public Color getFontColor() {
+        return new Color(value < 16 ? 0x776e65 : 0xf9f6f2);
+    }
+
+    public Color getTileColor() {
+        int color;
+        switch (value) {
+            case 0: color = (0xcdc1b4); break;
+            case 2: color = (0xeee4da); break;
+            case 4: color = (0xede0c8); break;
+            case 8: color = (0xf2b179); break;
+            case 16: color = (0xf59563); break;
+            case 32: color = (0xf67c5f); break;
+            case 64: color = (0xf65e3b); break;
+            case 128: color = (0xedcf72); break;
+            case 256: color = (0xedcc61); break;
+            case 512: color = (0xedc850); break;
+            case 1024: color = (0xedc53f); break;
+            case 2048: color = (0xedc22e); break;
+            default: color = (0xff0000);
+        }
+        return new Color(color);
+    }
+}
