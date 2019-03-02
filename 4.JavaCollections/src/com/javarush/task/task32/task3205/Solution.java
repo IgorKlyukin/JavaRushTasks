db6a71024eed312bbc39b9392d7a2a26b0674a01
Igor <igor_pol04@mail.ru>
2019-03-02 11:52:08 +0500

    Создание прокси-объекта 1) В отдельном файле создай публичный класс CustomInvocationHandler, который будет хэндлером при создании прокси-объекта. 2) CustomInvocationHandler должен поддерживать интерфейс InvocationHandler. 3) CustomInvocationHandler должен иметь один публичный конструктор с одним аргументом типа SomeInterfaceWithMethods. 4) Перед вызовом любого метода у оригинального объекта должна выводиться фраза [methodName in]. 5) После вызова любого метода у оригинального объекта должна выводиться фраза [methodName out]. 6) Реализуй логику метода getProxy, который должен создавать прокси (Proxy.newProxyInstance(...)). См. пример вывода в методе main. Метод main не участвует в тестировании.
    Требования:
    1. Класс CustomInvocationHandler должен существовать.
    2. Класс CustomInvocationHandler должен поддерживать интерфейс InvocationHandler.
    3. Класс CustomInvocationHandler должен иметь один публичный конструктор с одним аргументом типа SomeInterfaceWithMethods.
    4. Перед вызовом любого метода у оригинального объекта должна выводиться фраза [methodName in].
    5. После вызова любого метода у оригинального объекта должна выводиться фраза [methodName out].
    6. Метод getProxy должен создавать прокси для интерфейса SomeInterfaceWithMethods.

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3205/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task32.task3205;
+
+import java.lang.reflect.Proxy;
+
+/*
+Создание прокси-объекта
+*/
+public class Solution {
+    public static void main(String[] args) {
+        SomeInterfaceWithMethods obj = getProxy();
+        obj.stringMethodWithoutArgs();
+        obj.voidMethodWithIntArg(1);
+
+        /* expected output
+        stringMethodWithoutArgs in
+        inside stringMethodWithoutArgs
+        stringMethodWithoutArgs out
+        voidMethodWithIntArg in
+        inside voidMethodWithIntArg
+        inside voidMethodWithoutArgs
+        voidMethodWithIntArg out
+        */
+    }
+
+    public static SomeInterfaceWithMethods getProxy() {
+        SomeInterfaceWithMethods impl = new SomeInterfaceWithMethodsImpl();
+        ClassLoader classLoader = impl.getClass().getClassLoader();
+        Class[] interfaces = impl.getClass().getInterfaces();
+        return (SomeInterfaceWithMethods)Proxy.newProxyInstance(classLoader, interfaces, new CustomInvocationHandler(impl));
+    }
+}
