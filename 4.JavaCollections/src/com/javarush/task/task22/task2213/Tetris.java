db68b467e37ed2fe723b610fbb5e86e9e6c951a9
Igor <igor_pol04@mail.ru>
2019-03-03 13:05:53 +0500

    Тетрис(3) Поле есть, фигура есть, а как их получить?
    Добавь геттеры для полей field и figure.
    
    Требования:
    1. В классе Tetris должен быть создан метод getField без параметров.
    2. В классе Tetris должен быть создан метод getFigure без параметров.
    3. Метод getField должен быть объявлен с самым широким типом доступа.
    4. Метод getFigure должен быть объявлен с самым широким типом доступа.
    5. Метод getField должен возвращать значение поля field.
    6. Метод getFigure должен возвращать значение поля figure.

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -3,6 +3,15 @@ package com.javarush.task.task22.task2213;
 public class Tetris {
     private Field field;
     private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
 
     }
