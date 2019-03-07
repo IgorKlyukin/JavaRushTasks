eec520e97e3741f4706053c5999109219fa2bc7b
Igor <igor_pol04@mail.ru>
2019-03-07 20:38:59 +0500

    Тетрис(7) Нам понадобится еще 4 метода в классе Field: а) print() - объект будет отрисовывать на экран свое текущее состояние; б) removeFullLines() - будет удалять из матрицы полностью заполненные строки и сдвигать вышележащие строки вниз; в) Integer getValue(int x, int y) - возвращает значение которое находится в матрице с координатами x и y; г) void setValue(int x, int y, int value) - устанавливает переданное значение в ячейку массива (матрицы) с координатами x, y.
    Требования:
    1. В классе Field должен быть создан метод print без параметров.
    2. В классе Field должен быть создан метод removeFullLines без параметров.
    3. В классе Field должен быть создан метод getValue с двумя параметрами типа int.
    4. В классе Field должен быть создан метод setValue c тремя параметрами типа int.
    5. Метод print не должен возвращать значение.
    6. Метод removeFullLines не должен возвращать значение.
    7. Метод getValue должен возвращать значение типа Integer.
    8. Метод setValue не должен возвращать значение.

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task22.task2213;
 
+import java.util.ArrayList;
+
 public class Field {
     private int width; //ширина
     private int height; //высота
@@ -22,5 +24,46 @@ public class Field {
 
     public int[][] getMatrix() {
         return matrix;
+    }
+
+    public void print() {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void removeFullLines() {
+        boolean flag;
+        ArrayList<Integer> zeroList = new ArrayList<>();
+        for (int i = 0; i < height; i++) {
+            flag = false;
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 0)
+                    break;
+                if (j == width - 1)
+                    flag = true;
+            }
+            if (flag)
+                zeroList.add(i);
+        }
+
+        for (int i = 0; i < zeroList.size(); i++) {
+            for (int j = zeroList.get(i); j > 0; j--) {
+                for (int k = 0; k < width; k++) {
+                    matrix[j][k] = matrix[j - 1][k];
+                }
+            }
+        }
+    }
+
+    public Integer getValue(int x, int y) {
+        return matrix[y][x];
+    }
+
+    public void setValue(int x, int y, int value) {
+        matrix[y][x] = value;
     }
 }
