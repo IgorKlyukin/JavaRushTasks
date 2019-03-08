d5436805c9b703129d16405dcda811257a13ec0f
Igor <igor_pol04@mail.ru>
2019-03-08 13:28:01 +0500

    Тетрис(10) Также нам понадобятся методы для управления фигуркой.
    Добавь в класс Figure методы:
    left() - для движения фигурки влево.
    right() - для движения фигурки вправо.
    down() - для движения фигурки вниз.
    up() - для движения фигурки вверх.
    rotate() - для поворота фигурки вокруг главной диагонали.
    downMaximum() - падение фигурки в низ до дна.
    boolean isCurrentPositionAvailable() - проверка - может ли фигурка быть помещена в текущую позицию. Для теста захардкодь результат в true, пока мы не реализовали логику.
    landed() - вызывается, когда фигурка достигла дна или уперлась в другую фигурку
    Все ее занятые клетки теперь должны добавиться в Field.
    
    Требования:
    1. В классе Figure должен быть создан метод left без параметров.
    2. В классе Figure должен быть создан метод right без параметров.
    3. В классе Figure должен быть создан метод down без параметров.
    4. В классе Figure должен быть создан метод up без параметров.
    5. В классе Figure должен быть создан метод rotate без параметров.
    6. В классе Figure должен быть создан метод downMaximum без параметров.
    7. В классе Figure должен быть создан метод isCurrentPositionAvailable без параметров.
    8. В классе Figure должен быть создан метод landed без параметров.
    9. Метод isCurrentPositionAvailable должен возвращать true.

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -22,4 +22,42 @@ public class Figure {
     public int[][] getMatrix() {
         return matrix;
     }
+
+    public void left() {
+        x--;
+    }
+
+    public void right() {
+        x++;
+    }
+
+    public void down() {
+        y++;
+    }
+
+    public void up() {
+        y--;
+    }
+
+    public void rotate() {
+        int t;
+        for (int i = 0; i < 2; i++) {
+            t = matrix[0][i];
+            matrix[0][i] = matrix[2 - i][0];
+            matrix[2 - i][0] = matrix[2][2 - i];
+            matrix[2][2 - i] = matrix[i][2];
+            matrix[i][2] = t;
+        }
+    }
+
+    public void downMaximum() {
+
+    }
+
+    public boolean isCurrentPositionAvailable() {
+        return true;
+    }
+
+    public void landed() {}
+
 }
