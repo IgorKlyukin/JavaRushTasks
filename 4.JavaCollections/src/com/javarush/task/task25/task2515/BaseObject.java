c62756c8f21e6f81c12a78a780b1b83c4a70cc75
Igor <igor_pol04@mail.ru>
2019-03-22 11:03:26 +0300

    Space (7) Теперь займемся классом Canvas. Он у нас будет содержать матрицу, куда мы будем рисовать. У матрицы есть ширина и высота. А еще будем в ней хранить не числа (int), а символы (char).
    Надо:
    а) Добавить в класс две переменные width и height;
    б) Добавить в класс переменную matrix (char[][]);
    в) Добавь геттеры для них;
    г) В конструкторе проинициализируй матрицу.
    
    Требования:
    1. В классе Canvas создай поле width. Добавь для него getter.
    2. В классе Canvas создай поле height. Добавь для него getter.
    3. В классе Canvas создай поле matrix (char[][]). Добавь для него getter.
    4. В классе Canvas создай конструктор Canvas(int width, int height). Инициализируй поля width и height.
    5. Инициализируй в конструкторе поле matrix (char[height][width]).

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,9 +1,15 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-    private double x;
-    private double y;
-    private double radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
@@ -37,23 +43,50 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
+    }
+
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
+    }
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
     public boolean isAlive() {
         return isAlive;
     }
 
-    public void move() {
-
-    }
-
-    public void draw() {
-
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
     public void die() {
         isAlive = false;
     }
 
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
     public boolean isIntersect(BaseObject o) {
-        return Math.sqrt(Math.pow(x - o.x, 2) + Math.pow(y - o.y, 2)) < Double.max(radius, o.radius);
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
