ce2296537328b7026ec9976fdee10c3e0bb0d8b7
Igor <igor_pol04@mail.ru>
2019-03-22 15:41:06 +0300

    Space (12) Теперь займемся кораблем. Для сложности сделаем так: если пользователь нажал кнопку влево, то корабль начинает все время двигаться влево. Если нажал кнопку вправо - все время в право до упора. Поэтому заведем специальную переменную dx, которая будет хранить направление движения корабля. если dx = 1, корабль каждый ход идет на 1 вправо, если dx = -1, корабль каждый ход идет на 1 влево.
    Надо:
    а) добавь конструктор, можно такой:
    Радиус корабля будет равен 3. Корабль большой - это вам не ракета и не бомба.
    public SpaceShip(double x, double y)
    {
    super(x, y, 3);
    }
    б) добавь в класс SpaceShip переменную dx (double, по умолчанию равна 0);
    в) метод moveLeft(), устанавливает dx равной -1;
    г) метод moveRight(), устанавливает dx равной 1.
    
    Требования:
    1. В классе SpaceShip конструктор должен принимать координаты x и y, а радиус выставлять всегда равный 3.
    2. В классе SpaceShip добавь поле dx (double).
    3. В классе SpaceShip напиши метод moveLeft(), который устанавливает поле dx равным -1.
    4. В классе SpaceShip напиши метод moveRight(), который устанавливает поле dx равным 1.

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,12 +1,16 @@
 package com.javarush.task.task25.task2515;
 
-public class Rocket extends BaseObject{
+/**
+ * Класс для объектов-ракета
+ */
+public class Rocket extends BaseObject {
+
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
     /**
-     * Отрисовываем себя на холсте.
+     * Метод рисует свой объект на "канвасе".
      */
     @Override
     public void draw(Canvas canvas) {
