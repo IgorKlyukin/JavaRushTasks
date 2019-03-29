8b74731026f25eb6f2823529b92b0998caaf4516
Igor <igor_pol04@mail.ru>
2019-03-29 17:17:37 +0300

    AmigoSet (3) Напиши свою реализацию следующих методов при условии, что нужно работать с ключами мапы: * Iterator<E> iterator() - очевидно, что это итератор ключей. Получи множество ключей в map, верни его итератор * int size() - это количество ключей в map, равно количеству элементов в map * boolean isEmpty() * boolean contains(Object o) * void clear() * boolean remove(Object o)
    Ничего своего писать не нужно, используй то, что уже реализовано для множества ключей map.
    Используй Alt+Insert => Override methods
    
    Требования:
    1. Метод iterator должен возвращать итератор для множества ключей поля map.
    2. Метод size должен возвращать то же, что и метод size поля map.
    3. Метод isEmpty должен возвращать true, если map не содержит ни одного элемента, иначе - false.
    4. Метод contains должен возвращать true, если map содержит анализируемый элемент, иначе - false.
    5. Метод clear должен вызывать метод clear объекта map.
    6. Метод remove должен удалять из map полученный в качестве параметра элемент.

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task37.task3707;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.*;
 
 public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneable, Set<E> {
@@ -19,12 +18,33 @@ public class AmigoSet<E> extends AbstractSet<E> implements Serializable, Cloneab
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        Iterator<E> iterable = map.keySet().iterator();
+        return iterable;
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return super.contains(o);
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return super.remove(o);
     }
 
     public boolean add(E e) {
