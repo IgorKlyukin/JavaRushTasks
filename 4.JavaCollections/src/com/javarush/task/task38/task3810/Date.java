c7e186b02a75d5447fea16ec38827e354aac618b
Igor <igor_pol04@mail.ru>
2019-05-10 19:57:16 +0300

    Реализуй аннотации Проект должен компилироваться и выводить аннотацию класса Solution. Класс Solution и его аннотацию не менять. Аннотация Changelog должна быть видна во время выполнения.
    Требования:
    1. Вывод на экран должен соответствовать ожидаемому.
    2. На экран должна быть выведена аннотация @Changelog класса Solution.
    3. Аннотация @Changelog должна быть видна во время выполнения программы.
    4. В классе Solution должен быть реализован метод main с одним параметром типа String[].

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Date.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Date {
+    //напиши свой код
+    short year();
+    byte month();
+    byte day();
+    byte hour();
+    byte minute();
+    byte second();
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Revision.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task38.task3810;
+
+public @interface Revision {
+    //напиши свой код
+    long revision();
+    Date date();
+    Author[] authors() default {};
+    String comment() default "";
+}
