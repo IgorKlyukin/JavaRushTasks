761cb1832b64fa3ee9cd6b199db4e701a38fdb6a
Igor <igor_pol04@mail.ru>
2019-05-20 21:57:28 +0300

    Sokoban (9) В процессе работы игры, будут возникать различные события. Давай создадим интерфейс слушателя событий EventListener. Его должен реализовывать каждый класс, который хочет обрабатывать события. А классы, которые будут генерировать события, будут вызывать методы этого интерфейса. 9.1. Добавь интерфейс EventListener в пакет controller. 9.2. Добавь в интерфейс void методы: 9.2.1. move(Direction direction) - передвинуть объект в определенном направлении. 9.2.2. restart() - начать заново текущий уровень. 9.2.3. startNextLevel() - начать следующий уровень. 9.2.4. levelCompleted(int level) - уровень с номером level завершён. 9.3. Добавь классу Controller интерфейс EventListener, напиши необходимые заглушки- реализации интерфейса. 9.4. Добавь в классы Model и Field по полю EventListener eventListener. 9.5. Добавь в классы Model, View и Field по методу setEventListener(EventListener eventListener). Этот метод в классе View должен вызвать аналогичный метод у объекта field, а в классах Model и Field устанавливать значение внутренних полей eventListener.
    Требования:
    1. Добавь интерфейс EventListener в пакет controller.
    2. Добавь в интерфейс EventListener void методы: move(Direction direction), restart(), startNextLevel() и levelCompleted(int level).
    3. Добавь классу Controller интерфейс EventListener, напиши необходимые заглушки-реализации интерфейса.
    4. Добавь в классы Model и Field по полю EventListener eventListener.
    5. Реализуй в классах Model, View и Field методы setEventListener(EventListener eventListener).

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -1,14 +1,21 @@
 package com.javarush.task.task34.task3410.view;
 
 
+import com.javarush.task.task34.task3410.controller.EventListener;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Field extends JPanel {
     private View view;
+    private EventListener eventListener;
 
     public Field(View view) {
         this.view = view;
+    }
+
+    public void setEventListener(EventListener eventListener) {
+        this.eventListener = eventListener;
     }
 
     public void paint(Graphics g) {
