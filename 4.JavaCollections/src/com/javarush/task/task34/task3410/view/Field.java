f4dacade42a41960d2165489861291ee58b1db3d
Igor <igor_pol04@mail.ru>
2019-05-21 11:52:57 +0300

    Sokoban (14) Добавим немного интерактивности в нашу игру (перемещение игрока с помощью клавиатуры). Начнем с обработки нажатия клавиш клавиатуры. 14.1. Добавь в класс Field вложенный класс KeyHandler унаследованный от KeyAdapter. 14.2. Перегрузи у него метод keyPressed(). Если была нажата клавиша с кодом VK_LEFT, то пошли eventListener-у событие move с параметром Direction.LEFT. Аналогичным образом обработай нажатия клавиш с кодом VK_RIGHT, VK_UP и VK_DOWN. Если пользователь нажал клавишу R с кодом VK_R, то вызови у слушателя событий метод restart(). 14.3. В конструкторе класса Field: 14.3.1. Создай объект класса KeyHandler. 14.3.2. Добавь его слушателем с помощью метода addKeyListener(). 14.3.3. Установи focusable в true (метод setFocusable()).
    Требования:
    1. Добавь в класс Field вложенный public класс KeyHandler унаследованный от KeyAdapter.
    2. В классе KeyHandler правильно реализуй метод keyPressed().
    3. В конструкторе класса Field создай объект класса KeyHandler.
    4. В конструкторе класса Field добавь объект класса KeyHandler слушателем с помощью метода addKeyListener().
    5. В конструкторе класса Field установи focusable в true (метод setFocusable()).

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/view/Field.java
@@ -2,10 +2,13 @@ package com.javarush.task.task34.task3410.view;
 
 
 import com.javarush.task.task34.task3410.controller.EventListener;
+import com.javarush.task.task34.task3410.model.Direction;
 import com.javarush.task.task34.task3410.model.GameObject;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Field extends JPanel {
     private View view;
@@ -13,6 +16,9 @@ public class Field extends JPanel {
 
     public Field(View view) {
         this.view = view;
+        KeyHandler keyHandler = new KeyHandler();
+        addKeyListener(keyHandler);
+        setFocusable(true);
     }
 
     public void setEventListener(EventListener eventListener) {
@@ -26,6 +32,35 @@ public class Field extends JPanel {
         for (GameObject game :
                 view.getGameObjects().getAll()) {
             game.draw(g);
+        }
+    }
+
+    public class KeyHandler extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT: {
+                    eventListener.move(Direction.LEFT);
+                    break;
+                }
+                case KeyEvent.VK_RIGHT: {
+                    eventListener.move(Direction.RIGHT);
+                    break;
+                }
+                case KeyEvent.VK_DOWN: {
+                    eventListener.move(Direction.DOWN);
+                    break;
+                }
+                case KeyEvent.VK_UP: {
+                    eventListener.move(Direction.UP);
+                    break;
+                }
+                case KeyEvent.VK_R: {
+                    eventListener.restart();
+                    break;
+                }
+            }
         }
     }
 }
