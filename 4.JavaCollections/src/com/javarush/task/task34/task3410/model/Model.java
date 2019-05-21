fabc666c9b37ce285df599e3008f7f733ffaff55
Igor <igor_pol04@mail.ru>
2019-05-21 10:42:29 +0300

    Sokoban (11) Начнем наполнять функционалом класс модели Model. Добавь в него: 11.1. Поле GameObjects gameObjects. Оно будет хранить наши игровые объекты. 11.2. Поле отвечающее за текущий уровень int currentLevel. Проинициализируй его значением 1. 11.3. Поле отвечающие за загрузчик уровней LevelLoader levelLoader. Проинициализируй его с помощью файла levels.txt из папки res. 11.4. Метод GameObjects getGameObjects(), он должен возвращать все игровые объекты. 11.5. Метод restartLevel(int level), он должен получать новые игровые объекты для указанного уровня у загрузчика уровня levelLoader и сохранять их в поле gameObjects. 11.6. Метод restart(), он должен перезапускать текущий уровень, вызывая restartLevel с нужным параметром. 11.7. Метод startNextLevel(), он должен увеличивать значение переменной currentLevel, хранящей номер текущего уровня и выполнять перезапуск нового уровня.
    Требования:
    1. Добавь в класс Model поле GameObjects gameObjects.
    2. Добавь в класс Model поле int currentLevel. Проинициализируй его значением 1.
    3. Добавь в класс Model поле LevelLoader levelLoader.
    4. Реализуй в классе Model метод GameObjects getGameObjects().
    5. Реализуй в классе Model метод restartLevel(int level).
    6. Реализуй в классе Model метод restart().
    7. Реализуй в классе Model метод startNextLevel().

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -2,11 +2,36 @@ package com.javarush.task.task34.task3410.model;
 
 import com.javarush.task.task34.task3410.controller.EventListener;
 
+import java.io.File;
+import java.nio.file.Paths;
+
 public class Model {
     public static final int FIELD_CELL_SIZE = 20;
     private EventListener eventListener;
-
+    private GameObjects gameObjects;
+    private int currentLevel = 1;
+//    private LevelLoader levelLoader = new LevelLoader(Paths.get(new File(this.getClass().getPackage().getName().replace(".", "\\")).getParent().replace("\\", ".") + ".res.levels.txt"));
+    private LevelLoader levelLoader = new LevelLoader(
+            new File(this.getClass().getPackage().getName()
+                + ".res.levels.txt").toPath());
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
+    }
+
+    public GameObjects getGameObjects() {
+        return gameObjects;
+    }
+
+    public void restartLevel(int level) {
+        gameObjects = levelLoader.getLevel(level);
+    }
+
+    public void restart() {
+        restartLevel(currentLevel);
+    }
+
+    public void startNextLevel() {
+        currentLevel++;
+        restart();
     }
 }
