99e4308b43f18d5ba37155d7b38dc2d4846d9d24
Igor <igor_pol04@mail.ru>
2019-05-21 15:27:15 +0300

    Sokoban (16) Осталось дописать реализацию загрузчика уровней. 16.1. Открой файл levels.txt и внимательно изучи структуру файла. Символ 'X' - означает стену, '*' - ящик, '.' - дом, '&' - ящик который стоит в доме, а '@' - игрока. Всего в файле 60 уровней. 16.2. Реализуй метод GameObjects getLevel(int level). Он должен: 16.2.1. Вычитывать из файла данные уровня level. Уровни должны повторяться циклически, если пользователь прошел все 60 уровней и попал на 61 уровень, то ему нужно вернуть 1, вместо 62 уровня вернуть 2 и т.д. 16.2.2. Создать все игровые объекты, описанные в указанном уровне. Координаты каждого игрового объекта должны быть рассчитаны согласно следующей логике: 16.2.2.1. Самый верхний левый объект (если такой есть) должен иметь координаты x = x0 = FIELD_CELL_SIZE / 2 и y = y0 = FIELD_CELL_SIZE / 2. 16.2.2.2. Объект, который находится на одну позицию правее от него должен иметь координаты x = x0 + FIELD_CELL_SIZE и y = y0. 16.2.2.3. Объект, который находится на одну позицию ниже от самого верхнего левого должен иметь координаты x = x0 и y = y0 + FIELD_CELL_SIZE. 16.2.2.4. Аналогично должны рассчитываться координаты любого объекта на поле. 16.2.3. Создать новое хранилище объектов GameObjects и поместить в него все объекты. 16.2.4. Вернуть созданное хранилище.
    Игра должна быть полностью рабочей. Проверь, если есть какие-то проблемы исправь их.
    
    Требования:
    1. Реализуй метод GameObjects getLevel(int level) согласно условию задачи.

--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/LevelLoader.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/LevelLoader.java
@@ -1,12 +1,15 @@
 package com.javarush.task.task34.task3410.model;
 
+import java.io.*;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Set;
 
 public class LevelLoader {
     private final Path levels;
+    //C:\Users\пк\JavaRushTasks\4.JavaCollections\src\com\javarush\task\task34\task3410\res\levels.txt
 
     public LevelLoader(Path levels) {
         this.levels = levels;
@@ -14,14 +17,62 @@ public class LevelLoader {
 
     public GameObjects getLevel(int level) {
         int fcs = Model.FIELD_CELL_SIZE / 2;
-        return  new GameObjects(new HashSet<>(Arrays.asList(new Wall(fcs, fcs),
-                                                            new Wall(fcs * 3, fcs),
-                                                            new Wall(fcs * 5, fcs))
-                                                            ),
-                                Collections.singleton(new Box(fcs * 3, fcs * 3)),
-                                Collections.singleton(new Home(fcs * 5, fcs * 3)),
-                                new Player(fcs, fcs * 3)
-                                );
+        Set<Wall> wallSet = new HashSet<>();
+        Set<Box> boxSet = new HashSet<>();
+        Set<Home> homeSet = new HashSet<>();
+        Player player = null;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(levels.toFile()));
+            while (reader.ready()){
+                String s = reader.readLine();
+                if (s.equals("Maze: " + ((level - 1) % 60 + 1))){
+                    while (!(s = reader.readLine()).equals(""));
+                    int j = 1;
+                    while (!(s = reader.readLine()).equals("")){
+                        for (int i = 0, n = s.length(); i < n; i++) {
+                            switch (s.charAt(i)){
+                                case 'X': {
+                                    wallSet.add(new Wall((i * 2 + 1) * fcs, j * fcs));
+                                    break;
+                                }
+                                case '*': {
+                                    boxSet.add(new Box((i * 2 + 1) * fcs, j * fcs));
+                                    break;
+                                }
+                                case '.': {
+                                    homeSet.add(new Home((i * 2 + 1) * fcs, j * fcs));
+                                    break;
+                                }
+                                case '&': {
+                                    boxSet.add(new Box((i * 2 + 1) * fcs, j * fcs));
+                                    homeSet.add(new Home((i * 2 + 1) * fcs, j * fcs));
+                                    break;
+                                }
+                                case '@': {
+                                    player = new Player((i * 2 + 1) * fcs, j * fcs);
+                                    break;
+                                }
+                                default: break;
+                            }
+                        }
+                        j+=2;
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new GameObjects(wallSet, boxSet, homeSet, player);
+//        return  new GameObjects(new HashSet<>(Arrays.asList(new Wall(fcs, fcs),
+//                                                            new Wall(fcs * 3, fcs),
+//                                                            new Wall(fcs * 5, fcs))
+//                                                            ),
+//                                Collections.singleton(new Box(fcs * 3, fcs * 3)),
+//                                Collections.singleton(new Home(fcs * 5, fcs * 3)),
+//                                new Player(fcs, fcs * 3)
+//                                );
 
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ 4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -12,10 +12,11 @@ public class Model {
     private EventListener eventListener;
     private GameObjects gameObjects;
     private int currentLevel = 1;
+    private LevelLoader levelLoader = new LevelLoader(Paths.get("C:\\Users\\пк\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task34\\task3410\\res\\levels.txt"));
 //    private LevelLoader levelLoader = new LevelLoader(Paths.get(new File(this.getClass().getPackage().getName().replace(".", "\\")).getParent().replace("\\", ".") + ".res.levels.txt"));
-    private LevelLoader levelLoader = new LevelLoader(
-            new File(this.getClass().getPackage().getName()
-                + ".res.levels.txt").toPath());
+//    private LevelLoader levelLoader = new LevelLoader(
+//            new File(this.getClass().getPackage().getName()
+//                + ".res.levels.txt").toPath());
     public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
